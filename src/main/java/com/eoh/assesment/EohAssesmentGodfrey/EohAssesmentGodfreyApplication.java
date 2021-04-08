627dcd66ae0fa7a7d94c0ffad29b4252eb73e1d3
MananaMM <80203490+MananaMM@users.noreply.github.com>
2021-04-08 18:53:36 +0200

    first commit

---
+++ src/main/java/com/eoh/assesment/EohAssesmentGodfrey/EohAssesmentGodfreyApplication.java
@@ -0,0 +1,13 @@
+package com.eoh.assesment.EohAssesmentGodfrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EohAssesmentGodfreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EohAssesmentGodfreyApplication.class, args);
+		
+	}
+}
---
+++ src/main/java/com/eoh/assesment/EohAssesmentGodfrey/invoice/InvoiceRepo.java
@@ -0,0 +1,7 @@
+package com.eoh.assesment.EohAssesmentGodfrey.invoice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InvoiceRepo extends CrudRepository<Invoice, Long>{
+
+}
