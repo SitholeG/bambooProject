0bbc870d8ed67e72aa5ad9f7dc058eeda3dd9995
MananaMM <80203490+MananaMM@users.noreply.github.com>
2021-04-09 19:17:04 +0200

    Fixing errors

--- src/main/java/com/eoh/assesment/EohAssesmentGodfrey/EohAssesmentGodfreyApplication.java
+++ src/main/java/com/eoh/assesment/EohAssesmentGodfrey/EohAssesmentGodfreyApplication.java
@@ -2,9 +2,16 @@ package com.eoh.assesment.EohAssesmentGodfrey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EohAssesmentGodfreyApplication {
+public class EohAssesmentGodfreyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EohAssesmentGodfreyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EohAssesmentGodfreyApplication.class, args);
